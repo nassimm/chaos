4fa946d4de116859e3c979eee6b6ae2f51e5f235
[nassimm] <[kortail@gmail.com]>
2020-03-20 16:12:31 +0100

    different color for itemlvl 60 and 70

--- helpers/item-count-to-loot-filter.js
+++ helpers/item-count-to-loot-filter.js
@@ -1,4 +1,4 @@
-const showItemClass = itemClass => 'Show\r\n\tRarity Rare\r\n\tClass "' +itemClass + '"\r\n\tSetFontSize 45\r\n\tSetTextColor 50 130 165 255\r\n\tSetBorderColor 50 130 165 255\r\n\tSetBackgroundColor 255 255 125 255\r\n\r\n'
+const showItemClass = itemClass => 'Show\r\n\tRarity Rare\r\n\tItemLevel >= 60\r\n\tItemLevel < 70\r\n\tClass "' +itemClass + '"\r\n\tSetFontSize 45\r\n\tSetTextColor 50 130 165 255\r\n\tSetBorderColor 50 130 165 255\r\n\tSetBackgroundColor 255 125 255 255\r\n\r\nShow\r\n\tRarity Rare\r\n\tClass "' +itemClass + '"\r\n\tSetFontSize 45\r\n\tSetTextColor 50 130 165 255\r\n\tItemLevel >= 60\r\n\tSetBorderColor 50 130 165 255\r\n\tSetBackgroundColor 255 255 125 255\r\n\r\n'
 
 const itemCountToLootFilter = itemCount => {
   let filterAdds = '';
