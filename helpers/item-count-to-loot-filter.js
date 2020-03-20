dc11e677c35f953b3c80a50795555bd3a7b5b9a3
[nassimm] <[kortail@gmail.com]>
2020-03-20 16:50:28 +0100

    further refactoring

--- helpers/item-count-to-loot-filter.js
+++ helpers/item-count-to-loot-filter.js
@@ -1,15 +1,15 @@
 const showItemClass = itemClass => 'Show\r\n\tRarity Rare\r\n\tItemLevel >= 60\r\n\tItemLevel < 70\r\n\tClass "' +itemClass + '"\r\n\tSetFontSize 45\r\n\tSetTextColor 50 130 165 255\r\n\tSetBorderColor 50 130 165 255\r\n\tSetBackgroundColor 255 125 255 255\r\n\r\nShow\r\n\tRarity Rare\r\n\tClass "' +itemClass + '"\r\n\tSetFontSize 45\r\n\tSetTextColor 50 130 165 255\r\n\tItemLevel >= 60\r\n\tSetBorderColor 50 130 165 255\r\n\tSetBackgroundColor 255 255 125 255\r\n\r\n'
 
-const itemCountToLootFilter = itemCount => {
+const itemCountToLootFilter = ({ringsInStash,amuletsInStash, commonItemsInStash }) => {
   let filterAdds = '';
-  if (itemCount.ringsInStash.count < 10) {
-    filterAdds = filterAdds + showItemClass(itemCount.ringsInStash.itemClass)
+  if (ringsInStash.count < 10) {
+    filterAdds = filterAdds + showItemClass(ringsInStash.itemClass)
   }
-  if (itemCount.amuletsInStash.count < 5) {
-    filterAdds = filterAdds + showItemClass(itemCount.amuletsInStash.itemClass)
+  if (amuletsInStash.count < 5) {
+    filterAdds = filterAdds + showItemClass(amuletsInStash.itemClass)
   }
-  if (itemCount.ringsInStash.count >= 10 && itemCount.amuletsInStash.count >= 5) {
-    filterAdds = filterAdds + itemCount.itemCount.filter(item => item.count < 10).map(item => showItemClass(item.itemClass)).join('\r\n')
+  if (ringsInStash.count >= 10 && amuletsInStash.count >= 5) {    
+    filterAdds = filterAdds + commonItemsInStash.filter(item => item.count < 10).map(item => showItemClass(item.itemClass)).join('\r\n')
   }
   return filterAdds;
 };
